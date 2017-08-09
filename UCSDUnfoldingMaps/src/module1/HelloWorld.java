502d3b501382e9c56618c30f400da6687a0f1f47
Taylor Huston <taylor.a.huston@gmail.com>
2017-08-08 22:40:17 -0700

    Complete Module 1

--- UCSDUnfoldingMaps/src/module1/HelloWorld.java
+++ UCSDUnfoldingMaps/src/module1/HelloWorld.java
@@ -73,8 +73,11 @@ public class HelloWorld extends PApplet
 		
 		// This line makes the map interactive
 		MapUtils.createDefaultEventDispatcher(this, map1);
-		
-		// TODO: Add code here that creates map2 
+
+		map2 = new UnfoldingMap(this, 550, 50, 350, 500, provider);
+		map2.zoomAndPanTo(zoomLevel, new Location(47.6f, -122.4f));
+
+		MapUtils.createDefaultEventDispatcher(this, map2);
 		// Then you'll modify draw() below
 
 	}
@@ -84,6 +87,7 @@ public class HelloWorld extends PApplet
 		// So far we only draw map1...
 		// TODO: Add code so that both maps are displayed
 		map1.draw();
+		map2.draw();
 	}
 
 	
